9b101d0dfde3455ded4abf25fa57e9b2230004ed
Dev.aa <dev.andresarias@gmail.com>
2025-08-13 21:30:37 -0500

    estructura base e imagen del fondo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<main className="min-h-screen flex items-center justify-center relative">
+			<div
+  				className="absolute inset-0 bg-cover bg-center filter blur-none"
+ 				style={{
+    			backgroundImage:
+     			 "url('https://images.unsplash.com/photo-1719153863464-b2ee7e28b6df?q=80&w=1169&auto=format&fit=crop')",
+  				}}
+				></div>
+
+		</main>
+	);
 };
 
 export default Login;
